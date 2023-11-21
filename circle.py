a858ec74b83393cba716a694988428501c78b677
Raf1ld <144904807+Raf1ld@users.noreply.github.com>
2023-11-21 23:42:35 +0300

    circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,26 @@
-import math
-
-
+from math import pi
+import unittest
 def area(r):
-    return math.pi * r * r
-
+    '''принимает число r, возвращает квадрат числа r умноженный на число π'''
+    if r < 0:
+        raise ValueError
+    return pi * r * r
 
 def perimeter(r):
-    return 2 * math.pi * r
+    '''принимает число r,возвращает удвоенное число r умноженное на число π'''
+    if r < 0:
+        raise ValueError
+    return 2 * pi * r
+    
+class cicleTestcase (unittest.TestCase):
+    def test_area_perimeter (self):
+        self.assertEqual(area(0),0)
+        self.assertEqual(area(1),pi)
+        self.assertEqual(area(3),pi*3*3)
+        self.assertEqual(perimeter(0),0)
+        self.assertEqual(perimeter(1),2*pi)
+        self.assertEqual(perimeter(3),2*pi*3)
 
+    def test_value (self):
+        self.assertEqual(ValueError,area,-2)
+        self.assertEqual(ValueError,perimeter,-2)
